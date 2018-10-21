1330d6be321626135da509b9b3edcdb579643e6e
unknown <m_mucek@wp.pl>
2018-10-21 16:35:29 +0200

    Sending and receiving emails with attachments using python

--- Examples/PythonEmailExample/EmailSendReceive.py
+++ Examples/PythonEmailExample/EmailSendReceive.py
@@ -1,58 +1,79 @@
 import email
-import poplib
 import smtplib
+import imaplib
 
-from email.message import EmailMessage
+# from email.message import EmailMessage
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 email_address = 'ksiegarnia.longinus@gmail.com'
 email_pass = 'Myszykiszki18'
 email_addressee = 'ksiegarnia.longinus@gmail.com'
 
 
-def send_test_message(sender: str, password: str, addressee: str, msg_number=''):
+def send_test_message(sender: str, password: str, addressee: str, msg_number='', files=[]):
     client = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     client.login(sender, password)
 
-    msg = EmailMessage()
+    msg = MIMEMultipart()
     msg['Subject'] = 'Python test message'
     msg['From'] = sender
     msg['To'] = addressee
-    msg.set_content("This is test message " + msg_number)
+    msg.attach(MIMEText("This is test message " + msg_number))
+
+    for file in files:
+        with open(file, 'rb') as attachment_file:
+            attachment = MIMEApplication(attachment_file.read())
+
+        attachment['Content-Disposition'] = 'attachment; filename="%s"' % file
+        msg.attach(attachment)
 
     client.send_message(msg)
     client.quit()
 
 
 def receive_test_message(user_email: str, password: str):
-    client = poplib.POP3_SSL('pop.gmail.com', 995)
-    client.user(user_email)
-    client.pass_(password)
+    con = imaplib.IMAP4_SSL('imap.gmail.com')
+    con.login(user_email, password)
+    con.select('INBOX')
 
-    messages = client.list()[1]
-    for i in range(len(messages)):
-        (client_msg, body, _) = client.retr(i+1)
-        for j in body:
-            msg = email.message_from_string(j.decode("utf-8"))
-            text = msg.get_payload()
-            if msg['Subject']:
-                print("Subject: " + msg['Subject'])
+    typ, data = con.search(None, 'ALL')
+    email_number = 0
+    for num in data[0].split():
+        typ, data = con.fetch(num, '(RFC822)')
+        email_number += 1
+        text = data[0][1]
+        msg = email.message_from_bytes(text)
 
-            if msg['From']:
-                print("From: " + msg['From'])
+        print("Subject: " + msg['Subject'])
+        print("From: " + msg['From'])
+        print("To: " + msg['To'])
+        print(msg.get_payload()[0].get_payload() + '\n')
 
-            if msg['To']:
-                print("To: " + msg['To'])
+        for part in msg.walk():
+            if part.get_content_maintype() == 'multipart':
+                continue
+            if part.get('Content-Disposition') is None:
+                continue
+            filename = part.get_filename()
+            data = part.get_payload(decode=True)
+            if not data:
+                continue
+            f = open('attachment' + str(email_number) + filename, 'w')
+            data = str(data).strip('\'').strip('b\'')
+            f.write(data)
+            f.close()
 
-            if text:
-                print(text + '\n')
-
-    client.quit()
+    con.close()
+    con.logout()
 
 
 def main():
-    for i in range(5):
+    for i in range(2):
         message_number = str(i + 1)
-        send_test_message(email_address, email_pass, email_addressee, msg_number=message_number)
+        send_test_message(email_address, email_pass, email_addressee,
+                          msg_number=message_number, files=['ExampleAttachment.txt'])
     receive_test_message(email_address, email_pass)
 
 
