dc8d9c69da360ad113fc4cc693b796010e444dee
Sunnivran <piotrek-zakowski@wp.pl>
2018-11-05 21:57:06 +0100

    Added C# library for sending & receiving emails. Added example of usage in GUI for WorkersSelectionWindow.

---
+++ Examples/CSharpEmailLibrary/CSharpEmailLibrary/MailReceiver.cs
@@ -0,0 +1,33 @@
+﻿using OpenPop.Mime;
+using OpenPop.Pop3;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpEmailLibrary
+{
+    public class MailReceiver
+    {
+        private const string popServerAddress = "pop.gmail.com";
+        private const int popServerPort = 995;
+
+        private Pop3Client client;
+
+        public MailReceiver(string mailAddress, string mailPassword)
+        {
+            this.client = new Pop3Client();
+            this.client.Connect(popServerAddress, popServerPort, true);
+            this.client.Authenticate(mailAddress, mailPassword);
+        }
+
+        public Mail GetLastMessage()
+        {
+            int lastMessageNumber = client.GetMessageCount();
+            Message message = client.GetMessage(lastMessageNumber);
+
+            Mail receivedMail = new Mail(message);
+
+            return receivedMail;
+        }
+    }
+}
---
+++ Examples/CSharpEmailLibrary/CSharpEmailLibrary/MailSender.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace CSharpEmailLibrary
+{
+    public class MailSender
+    {
+        private const string smtpServerAddress = "smtp.gmail.com";
+        private const int smtpServerPort = 587;
+
+        private SmtpClient client;
+
+        public MailSender(string mailAddress, string mailPassword)
+        {
+            this.client = new SmtpClient(smtpServerAddress, smtpServerPort)
+            {
+                Credentials = new NetworkCredential(mailAddress, mailPassword),
+                EnableSsl = true
+            };
+        }
+
+        public void SendMail(Mail mailToSend)
+        {
+            try
+            {
+                client.Send(mailToSend.GetMessageToSend());
+            }
+            catch (SmtpException ex)
+            {
+                throw new ApplicationException
+                  ("SmtpException has occured: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
