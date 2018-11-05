368ba7b697ea8415c797f8ff278aa36eb570c317
Joanna Jankowska <joanna.94@op.pl>
2018-11-05 18:02:04 +0100

    added c# class to work on XPDL file

---
+++ Examples/XPDLReader/XPDLReader/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XPDLReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //przykladowe wywolania funkcji
+            XPDLManager xpdl = new XPDLManager("xpdl.xml");
+            
+            string []next = new string[3];
+            next = xpdl.GetNext("ADDING_PRODUCTS_RESPONSE");
+            Console.WriteLine(next[0]);
+
+            xpdl.SetCurrentActivity("RESPONSE_TO_ARCHIVE");
+
+            Console.WriteLine(xpdl.GetPreviousActivity());
+            next = xpdl.GetNext();
+            Console.WriteLine(next[0]);
+
+            Console.WriteLine(xpdl.GetActivityName("CLIENT_OFFER_REJECTED"));
+
+
+
+        }
+    }
+}
