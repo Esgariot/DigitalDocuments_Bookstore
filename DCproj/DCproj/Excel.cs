73e6ce4ceaba752efc332b8dad75cbf7073d6991
tpolakow <30654616+tpolakow@users.noreply.github.com>
2018-11-19 20:29:53 +0100

    Add files via upload

---
+++ DCproj/DCproj/Excel.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace DCproj
+{
+    class Excel
+    {
+        public string path = "";
+        _Application excel = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+        private readonly int col = 6;
+
+        public Excel(string path, int sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[sheet];
+        }
+
+        public string ReadCell(int i, int j)
+        {
+            i++;
+            j++;
+            if (ws.Cells[i, j].Value2 != null)
+                return ws.Cells[i, j].Value2;
+            else
+                return "";
+        }
+
+        public int NumOfRows()
+        {
+            int i = 0;
+            bool end = false;
+            if (ws.Cells[1, 1].Value2 == null)
+                return 0;
+
+            while (end != true)
+            {
+                i++;
+                if (ws.Cells[i, 1].Value2 == null && ws.Cells[i+1, 1].Value2 == null)
+                {
+                    end = true;
+                }
+            }
+            return i;
+        }
+
+        public int NumOfHeaderRows()
+        {
+            int i = 0;
+            bool end = false;
+            if (ws.Cells[1, 1].Value2 == null)
+                return 0;
+
+            while (end != true)
+            {
+                i++;
+                if (ws.Cells[i, 1].Value2 == "Products")
+                {
+                    i++;
+                    end = true;
+                }
+            }
+            return i;
+        }
+
+        public string[,] ReadRange(int starti, int startj, int endi, int endj)
+        {
+            Range range = (Range)ws.Range[ws.Cells[starti, startj], ws.Cells[endi, endj]];
+            object[,] holder = range.Value2;
+            string[,] returnString = new string[endi - starti, endj - startj];
+            for(int i=1; i<=endi-starti; i++)
+            {
+                for (int j = 1; j <= endj - startj; j++)
+                {
+                    if (holder[i, j] != null)
+                        returnString[i - 1, j - 1] = holder[i, j].ToString();
+                    else
+                        returnString[i - 1, j - 1] = "";
+                }
+            }
+            return returnString;
+        }
+
+
+        public void WriteRange(int starti, int startj, int endi, int endj, string[,] writeString)
+        {
+            Range range = (Range)ws.Range[ws.Cells[starti, startj], ws.Cells[endi, endj]];
+            range.Value2 = writeString;
+        }
+
+        public void WriteCell(int i, int j, string value)
+        {
+            i++;
+            j++;
+            ws.Cells[i, j].Value2 = value;
+        }
+
+        public void Save()
+        {
+            wb.Save();
+        }
+
+        public void SaveAs(string path)
+        {
+            wb.SaveAs(path);
+        }
+
+        public void Close()
+        {
+            wb.Close();
+        }
+    }
+}
---
+++ DCproj/DCproj/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DCproj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //OpenFile();
+            Excel excelA = new Excel(@"C:\Users\Tomasz\Desktop\studia\DC\A.csv", 1);
+            Excel excelB = new Excel(@"C:\Users\Tomasz\Desktop\studia\DC\B.csv", 1);//dest
+            int numOfHeaderRows = excelA.NumOfHeaderRows();
+            int numOfRowsA = excelA.NumOfRows();
+            int numOfRowsB = excelB.NumOfRows();
+            string[,] readString = excelA.ReadRange(numOfHeaderRows+1, 1, numOfRowsA, 7);
+            excelB.WriteRange(numOfRowsB, 1, numOfRowsA- (numOfHeaderRows + 1) + numOfRowsB - 1, 6, readString);
+            excelB.SaveAs(@"C:\Users\Tomasz\Desktop\studia\DC\TestFiles\Test1.csv");
+            excelA.Close();
+            excelB.Close();
+
+        }
+
+        public void OpenFile()
+        {
+            Excel excel = new Excel(@"C:\Users\Tomasz\Desktop\studia\DC\Sample.xlsx", 1);
+
+            MessageBox.Show(excel.ReadCell(0, 0));
+        }
+
+
+    }
+}
